6f676a366fee670e911ef6b03337c387d1b13f2c
Miguel Torres <mikeajr@icloud.com>
2023-11-10 22:06:53 -0600

    product component

--- src/components/ProductCards.tsx
+++ src/components/ProductCards.tsx
@@ -21,6 +21,43 @@ function imageFilter(
 	return image && `${image.url}&width=${width}&height=${height}`;
 }
 
+interface ProductProps {
+	product: z.infer<typeof ProductResult>;
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+	return (
+		<div className="flex flex-col justify-between items-center rounded-md border-2 p-4 border-neutral-200">
+			<h1 className="text-xl font-thin text-center">{product?.title}</h1>
+			<img
+				src={product?.featuredImage?.url}
+				alt={product?.featuredImage?.altText || ""}
+				srcSet={srcSetValues
+					.filter(
+						(value) =>
+							value < (product?.featuredImage?.width || 200)
+					)
+					.map((value) => {
+						if ((product?.featuredImage?.width || 200) >= value) {
+							if (product)
+								return `${imageFilter(product?.featuredImage, {
+									width: value,
+								})} ${value}w`;
+						}
+					})
+					.join(", ")
+					.concat(
+						`, ${product?.featuredImage?.url} ${product?.featuredImage?.width}w`
+					)}
+			/>
+			<Button className="px-2" variant="outline">
+				<PlusSquare strokeWidth={1.0} className="w-4 h-4 mr-1" />
+				Add to Cart
+			</Button>
+		</div>
+	);
+};
+
 interface ProductCardsProps {
 	products: z.infer<typeof ProductsResult>;
 }
@@ -31,47 +68,7 @@ const ProductCards: React.FC<ProductCardsProps> = ({ products }) => {
 			<MaxWidthWrapper className="min-h-screen">
 				<div className="grid grid-cols-4 gap-6">
 					{products.map((product) => (
-						<div className="flex flex-col justify-between items-center rounded-md border-2 p-4 border-neutral-200">
-							<h1 className="text-xl font-thin text-center">
-								{product?.title}
-							</h1>
-							<img
-								src={product?.featuredImage?.url}
-								alt={product?.featuredImage?.altText || ""}
-								srcSet={srcSetValues
-									.filter(
-										(value) =>
-											value <
-											(product?.featuredImage?.width ||
-												200)
-									)
-									.map((value) => {
-										if (
-											(product?.featuredImage?.width ||
-												200) >= value
-										) {
-											if (product)
-												return `${imageFilter(
-													product?.featuredImage,
-													{
-														width: value,
-													}
-												)} ${value}w`;
-										}
-									})
-									.join(", ")
-									.concat(
-										`, ${product?.featuredImage?.url} ${product?.featuredImage?.width}w`
-									)}
-							/>
-							<Button className="px-2" variant="outline">
-								<PlusSquare
-									strokeWidth={1.0}
-									className="w-4 h-4 mr-1"
-								/>
-								Add to Cart
-							</Button>
-						</div>
+						<Product product={product} />
 					))}
 				</div>
 			</MaxWidthWrapper>
